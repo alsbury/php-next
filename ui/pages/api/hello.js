60f32b7a2079245b4dc74f946819404be6ae84a7
David Alsbury <david@alsbury.com>
2022-03-25 22:17:13 -0500

    adding ui

---
+++ ui/pages/api/hello.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function hello(req, res) {
+  res.status(200).json({
+    name: 'John James Doe',
+    foo: 'bar',
+    date: (new Date()).valueOf()
+  })
+}
